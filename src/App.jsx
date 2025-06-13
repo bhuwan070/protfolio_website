3a6b4c7678542455edec463ed9cc03633df4fe14
bhuwanoj_ <bhuwanojha10@gmail.com>
2025-06-13 20:39:57 +0545

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Suspense } from "react";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
+import routes from "./routes";
 import { ToastContainer } from "react-toastify";
 
 export default function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <RouterProvider router={router} />
+      <RouterProvider router={routes} />
       <ToastContainer />
     </Suspense>
   );
--- src/routes.jsx
+++ src/routes.jsx
@@ -6,13 +6,13 @@ const Home = lazy(() => import("./pages/Home"));
 const About = lazy(() => import("./pages/About"));
 const Priorities = lazy(() => import("./pages/Priorities"));
 const Inmedia = lazy(() => import("./pages/Inmedia"));
-const Contact = lazy(() => import("./pages/Contact"));
+const Contact = lazy(() => import("./pages/Contacts"));
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <LandingLayout />,
-    childern: [
+    children: [
       { index: true, element: <Home /> },
       { path: "about", element: <About /> },
       { path: "priorities", element: <Priorities /> },
